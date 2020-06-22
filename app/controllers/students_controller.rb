c59373b283540a9f1a92d9fc638701cf8444791e
Aniss Marrouch <aniss.marrouch@gmail.com>
2020-06-22 19:21:57 +0000

    Automatically backed up by Learn

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,10 @@
+class StudentsController < ApplicationController
+  before_action :set_student, only: [:show, :edit, :update, :destroy]
+
+  # GET /students
+  # GET /students.json
+  def index
+    @students = Student.all
+  end
+
+end
